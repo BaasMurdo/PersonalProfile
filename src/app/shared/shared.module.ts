d7cc076a9b54fd20a6df3138e7975abb1a875651
BaasMurdo <BaasMurdo@Gmail.com>
2022-12-08 19:21:57 +0200

    Added some components and some nesting

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { ExplorerComponent } from './components/explorer/explorer.component';
+import { TabsBarComponent } from './components/tabs-bar/tabs-bar.component';
+import { BottomBarComponent } from './components/bottom-bar/bottom-bar.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 
 
 @NgModule({
   declarations: [
-    TopNavComponent
+    TopNavComponent,
+    SideBarComponent,
+    ExplorerComponent,
+    TabsBarComponent,
+    BottomBarComponent,
+    LayoutComponent
   ],
   imports: [
     CommonModule
