b614d8acc4fdfe67a3ac1e77b792598593001453
BaasMurdo <BaasMurdo@Gmail.com>
2022-12-12 23:54:58 +0200

    added tabs

---
+++ src/app/shared/components/tab/tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrls: ['./tab.component.scss']
+})
+export class tabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
